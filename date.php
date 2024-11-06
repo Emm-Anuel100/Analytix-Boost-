d3330396d10af4d073002c4e971fb82373ae19df
Emm-Anuel100 <chinweokwuemmanuel2004@gmail.com>
2024-11-06 18:32:15 +0100

    Readme.md file update

--- date.php
+++
@@ -1,9 +0,0 @@
-<?php 
-
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $startDate = $_POST['startDate'];  // Start date selected by the user
-    $endDate = $_POST['endDate'];      // End date selected by the user
-
-    // Process the dates, e.g., querying the database for data within this range
-    echo "Selected Date Range: $startDate to $endDate";
-}
